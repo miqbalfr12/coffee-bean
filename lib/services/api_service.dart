b563726b34391fcc8583bc3bf7472754a6d19005
AlexVinzt <rafi22613@gmail.com>
2024-07-07 17:00:54 +0700

    feat(api): post product, payment + put product, delete product

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -48,4 +48,65 @@ class ApiService {
       throw Exception('Failed to load payment');
     }
   }
+
+  Future<http.Response> createProduct(String name, String category, int price,
+      String description, int stock, List<String> materials) async {
+    var response = await http.post(
+      Uri.parse('$baseUrl/product'),
+      body: json.encode({
+        'name': name,
+        'category': category,
+        'price': price,
+        'description': description,
+        'stock': stock,
+        'materials': materials,
+      }),
+      headers: {'Content-Type': 'application/json'},
+    );
+    print(response.statusCode);
+    return response;
+  }
+
+  Future<http.Response> editProduct(String id, String name, String category,
+      int price, String description, int stock, List<String> materials) async {
+    var response = await http.put(
+      Uri.parse('$baseUrl/product/$id'),
+      body: json.encode({
+        'name': name,
+        'category': category,
+        'price': price,
+        'description': description,
+        'stock': stock,
+        'materials': materials,
+      }),
+      headers: {'Content-Type': 'application/json'},
+    );
+    print(response.statusCode);
+    return response;
+  }
+
+  Future<http.Response> deleteProduct(String id) async {
+    var response = await http.delete(
+      Uri.parse('$baseUrl/product/$id'),
+      headers: {'Content-Type': 'application/json'},
+    );
+    print(response.statusCode);
+    return response;
+  }
+
+  Future<http.Response> createPayment(
+      int total, String consumer, String createdBy, List<Order> order) async {
+    var response = await http.post(
+      Uri.parse('$baseUrl/payment'),
+      body: json.encode({
+        'total': total,
+        'consumer': consumer,
+        'createdBy': createdBy,
+        'order': order.map((o) => o.toJson()).toList(),
+      }),
+      headers: {'Content-Type': 'application/json'},
+    );
+    print(response.statusCode);
+    return response;
+  }
 }
